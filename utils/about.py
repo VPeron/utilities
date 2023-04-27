55476c6de2ca0c37205b2cc968c56bc7fe70d47f
Vinicius Peron <viperon.python@gmail.com>
2023-04-27 10:05:34 +0200

    minor UI changes, most notably starting video presentation on the correct time stamp for the cc detector

--- utils/about.py
+++ utils/about.py
@@ -6,4 +6,4 @@ def profile():
     st.info('My Github account: https://github.com/VPeron')
     st.write('Here you can watch my presentation for the Lewagon Data Science Capstone project: Chess Engine Detection (skip to 36m27s)')
 
-    st.video('https://www.youtube.com/watch?v=d_SOksxtVzs&t=2197s')
+    st.video('https://www.youtube.com/watch?v=d_SOksxtVzs&t=2197s', start_time=2197)
