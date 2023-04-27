3c8f7c141840528b5067ac833a21c5bc956eff0f
Vinicius Peron <viperon.python@gmail.com>
2023-04-27 15:53:53 +0200

    reinstsating logger with console display rather than file writing

--- utils/custom_logger.py
+++ utils/custom_logger.py
@@ -6,8 +6,8 @@ def get_custom_logger(name):
     """
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    # create a file handler
-    handler = logging.FileHandler('logs/' + name + '.log')
+    # create a stream handler to print logs to console
+    handler = logging.StreamHandler()
     handler.setLevel(logging.INFO)
     # create a logging format
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
