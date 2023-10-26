643810c9760027a23ae7cb7052cea904fd11858d
Vinicius Peron <viperon.python@gmail.com>
2023-10-26 22:22:25 +0200

    adding ironhack deliverable to references

--- utils/about.py
+++ utils/about.py
@@ -12,6 +12,7 @@ They have been created for my personal use and to showcase some of my programs.
     st.write('[My Linkedin](https://www.linkedin.com/in/vinicius-p-9a9197270/)')
     st.write('Here you can watch my presentation for the Lewagon Data Science Bootcamp Capstone project: Chess Engine Detection')
     st.video('https://www.youtube.com/watch?v=d_SOksxtVzs&t=2197s', start_time=2197)
+    st.markdown("[Here's my Ironhack CyberSecurity Course Deliverable](https://ironhackdeliverable-group5.streamlit.app/)")
     st.markdown("""
 # Holopin Badges
 [![@vperon's Holopin badges full Holopin profile](https://holopin.me/vperon)](https://holopin.io/@vperon)
